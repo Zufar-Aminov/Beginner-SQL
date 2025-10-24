a5ab52604336f14ba48923285fbe5ebf9940638d
Zufar <aminovzufar601@gmail.com>
2025-10-24 18:41:34 +0300

    SQL

---
+++ Beginner SQL  .sql
@@ -0,0 +1,21 @@
+CREATE TABLE Oquvchilar(
+Ismi varchar(50),
+Sinfi int,
+Yoshi int,
+Maktabi int);
+
+SELECT * FROM Oquvchilar
+
+INSERT INTO Oquvchilar (Ismi,Sinfi,Yoshi,Maktabi)
+VALUES('Zufar',7,13,33);
+
+SELECT * FROM Oquvchilar
+
+INSERT INTO Oquvchilar (Ismi,Sinfi,Yoshi,Maktabi)
+VALUES
+('Gulora' ,7,13,33),
+('Aziz' ,7,12,33),
+('Dilafruz' ,7,13,33),
+('Nematjon' ,7,13,33);
+
+SELECT * FROM Oquvchilar
